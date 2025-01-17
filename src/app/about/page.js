a3baa908aac6f21d852128c879a8f7b34573a572
Sebastian Ignacio Pino Marcano <sebaspino2908@gmail.com>
2025-01-16 22:59:17 -0300

    a

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+    return (
+      <section>
+        <h1>Sobre Nós</h1>
+        <p>Informações sobre a empresa ou projeto.</p>
+      </section>
+    );
+  }
